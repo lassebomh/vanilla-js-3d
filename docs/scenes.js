d463531c525cfe8d17394b0cc599025ae3a39ccb
Lasse H. Bomholt <lasse@bomh.net>
2025-08-16 19:26:36 +0200

    look at mouse

--- docs/scenes.js
+++ docs/scenes.js
@@ -39,13 +39,42 @@ export function bomhnet() {
   const light_direction = Matrix.forward.mul(rotate_x(0.9)).mul(rotate_y(0.4));
   const light_direction_inv = light_direction.neg();
 
+  let mouse_x = 0;
+  let mouse_y = 0;
+
+  let canvas_x = 0;
+  let canvas_y = 0;
+
+  let camera_pitch = 0;
+  let camera_yaw = 0;
+
+  setInterval(() => {
+    canvas_x = ctx.canvas.clientLeft + ctx.canvas.clientWidth / 2;
+    canvas_y = ctx.canvas.clientTop + ctx.canvas.clientHeight / 2;
+  }, 500);
+
+  window.addEventListener(
+    "mousemove",
+    (e) => {
+      mouse_x = e.clientX;
+      mouse_y = e.clientY;
+    },
+    { passive: true }
+  );
+
   /**
    * @param {number} t
    */
   function frame(t) {
+    const target_camera_pitch = (mouse_x - canvas_x) / window.innerWidth;
+    const target_camera_yaw = (mouse_y - canvas_y) / window.innerHeight;
+
+    camera_pitch += (target_camera_pitch - camera_pitch) / 8;
+    camera_yaw += (target_camera_yaw - camera_yaw) / 8;
+
     let camera = translate(-0.1, 0.1, 0.8 + 1000 / ctx.canvas.width)
-      .mul(rotate_y(Math.cos(t / 1000) / 6 + 0.3))
-      .mul(rotate_x(Math.sin(t / 1000) / 3 + 0.1));
+      .mul(rotate_y(camera_pitch))
+      .mul(rotate_x(camera_yaw));
 
     let view = camera.inv();
     let view_projection = view.mul(projection);
